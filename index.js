ae566bae3f2fe0699d3f1a19aeb909838e8ea954
Somalinga <77963043+QwertyIsCoding@users.noreply.github.com>
2022-11-16 14:11:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -99,27 +99,7 @@ class Feed extends React.Component {
     });
   }
 
-  render() {
-    return (
-      <section className="feed">
-        <h1>Simple Load More/Pagination with React</h1>
-        <h2>With Array.prototype.slice() and the power of component state!</h2>
-
-        <div className="tiles" aria-live="polite">
-          {this.state.items.slice(0, this.state.visible).map((item, index) => {
-              return (
-                <div className="tile fade-in" key={item.id}>
-                  <span className="count">{index+1}</span>
-                  <h2>{item.title}</h2>
-                  <p>{item.body}</p>
-                </div>
-              );
-            })}
-          </div>
-          {this.state.visible < this.state.items.length &&
-             <button onClick={this.loadMore} type="button" className="load-more">Load more</button>
-          }
-        </section>
+  
     );
   }
 }
